9e82e9805d3f9067c621a646bab614e754f7e749
Valeriya Angelova <valeriya.angelova@quantori.com>
2022-03-28 21:10:59 +0300

    2110

--- services/web/main.py
+++ services/web/main.py
@@ -1,20 +1,10 @@
-import filler
+#from tamriel import filler
 from pandas import read_json
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from alchemy import Base, engine
+#from tamriel.alchemy import Base
 
 
-#engine = create_engine('postgresql+psycopg2://angelova:angelova@localhost:5432/tamriel_db')
-tamriel_heroes = read_json('tamriel1.json', encoding='UTF-8')
-Session = sessionmaker(bind=engine)
-
-#Base.metadata.create_all(engine)
-
-fill_db = filler.Fill_db(Session)
-
-#fill_db.fill_db_characters_auto()
-#fill_db.fill_battles()
-
-
-print(fill_db.__doc__)
+engine = create_engine('postgresql+psycopg2://angelova:angelova@localhost:5432/tamriel_db')
+conn=engine.connect()
+print(conn)
